d9ec1fe5989ba0e554e39ce45ced80bf3bb8bc01
maheat <144766658+maheat@users.noreply.github.com>
2023-12-14 19:06:24 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -16,42 +16,37 @@ window.addEventListener('scroll', () => {
     big_title.style.opacity = - scroll / (header_height / 2) + 1;
 })
 
+
 // Javascript to generate a slideshow for the image gallery when you click on it
 let slideIndex = 1;
 
-function openModal() {
-    document.getElementById('myModal').style.display = 'flex';
-}
-
-function closeModal() {
-    document.getElementById('myModal').style.display = 'none';
-}
-
-function plusSlides(n, galleryId) {
-    showSlides((slideIndex += n), galleryId);
-}
-
-function currentSlide(n, galleryId) {
-    showSlides((slideIndex = n), galleryId);
-}
-
-function showSlides(n, galleryId) {
-    let slides;
-    let modalImg = document.getElementById('modalImg');
-
-    if (galleryId === 'gallery') {
-        slides = document.getElementsByClassName('gallery-item');
-    } else if (galleryId === 'gallery2') {
-        slides = document.getElementsByClassName('gallery-item2');
+    function openModal() {
+        document.getElementById('myModal').style.display = 'flex';
     }
 
-    if (n > slides.length) {
-        slideIndex = 1;
+    function closeModal() {
+        document.getElementById('myModal').style.display = 'none';
     }
 
-    if (n < 1) {
-        slideIndex = slides.length;
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
     }
 
-    modalImg.src = slides[slideIndex - 1].getElementsByTagName('img')[0].src;
-}
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let slides = document.getElementsByClassName('gallery-item');
+        let modalImg = document.getElementById('modalImg');
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        modalImg.src = slides[slideIndex - 1].getElementsByTagName('img')[0].src;
+    }
